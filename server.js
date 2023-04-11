6774f9d20d1755b55924a99681cb53e9784fbcd6
Ruskin Acevedo <ruskin.acevedo@gmail.com>
2023-04-11 00:14:16 -0400

    Installed node-modules

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
+//Dependencies 
+const express = require('express');
+const mysql = require('mysql2'); /* Import and require mysql2*/
 
-const mysql = require('mysql12');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // Add MySQL password here
+        password: 'Prospect#20',
+        database: 'employers_db'
+    },
+    console.log(`Connected to the employers_db database.`)
+);
 
 
 
 
 
-
-
-
-
-
-// get the client
-const mysql = require('mysql2');
-
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test'
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
 });
 
-// execute will internally call prepare and query
-connection.execute(
-    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-    ['Rick C-137', 53],
-    function (err, results, fields) {
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-
-        // If you execute same statement again, it will be picked from a LRU cache
-        // which will save query preparation time and give better performance
-    }
-);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
